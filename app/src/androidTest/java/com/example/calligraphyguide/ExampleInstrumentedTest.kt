b653590eb70616593889497a5ee124b01b074add
Sidiney Rodrigues <4397607+sidineyr@users.noreply.github.com>
2026-08-24 14:58:11 -0300

    feat: finish CallGuide handwriting learning app

--- app/src/androidTest/java/com/example/calligraphyguide/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/calligraphyguide/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.calligraphyguide
+package com.sidineyr.callguide
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.calligraphyguide", appContext.packageName)
+        assertEquals("com.sidineyr.callguide", appContext.packageName)
     }
 }
