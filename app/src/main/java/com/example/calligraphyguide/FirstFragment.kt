c0f780cd276f0d8cf55e433f7db8fa9f989f62c0
Sidiney Rodrigues <4397607+sidineyr@users.noreply.github.com>
2026-08-24 14:58:22 -0300

    feat: finish CallGuide handwriting learning app

--- app/src/main/java/com/example/calligraphyguide/FirstFragment.kt
+++ app/src/main/java/com/example/calligraphyguide/FirstFragment.kt
@@ -1,44 +1,33 @@
-package com.example.calligraphyguide
+package com.sidineyr.callguide
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.calligraphyguide.databinding.FragmentFirstBinding
+import com.sidineyr.callguide.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class FirstFragment : Fragment() {
-
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, state: Bundle?): View {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+    override fun onViewCreated(view: View, state: Bundle?) {
+        binding.practiceButton.setOnClickListener {
+            findNavController().navigate(R.id.action_home_to_practice)
+        }
+        binding.guideButton.setOnClickListener {
+            findNavController().navigate(R.id.action_home_to_guide)
         }
     }
 
     override fun onDestroyView() {
-        super.onDestroyView()
         _binding = null
+        super.onDestroyView()
     }
 }
